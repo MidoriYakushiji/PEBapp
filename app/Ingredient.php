7dcbdfed5a4b392dd0bc2f6c93b7b5f7405177ff
MidoriYakushiji <m.yakushiji1008@gmail.com>
2022-09-28 11:37:44 +0900

    comment

--- app/Ingredient.php
+++ app/Ingredient.php
@@ -6,24 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ingredient extends Model
 {
-    public function getData()
+    public function getData()    //usersテーブルからnameを取得
     {
        return $this->user->name;
     }
     //$this->id . ': ' . $this->name . ' (' . $this->type . ')' . $this->price . $this->img .
     protected $guarded = array('id');
 
-    public static $rules = array(
-       'name' => 'required',
-       'type' => 'required|integer',
-       'price' => 'required|integer|between:0,2000',
-       'img' => 'required',
-       'user_id' => 'required|exists:users,id'
+    public static $rules = array(  
+       'name' => 'required',                         //必須
+       'type' => 'required|integer',                 //必須、整数
+       'price' => 'required|integer|between:0,2000', //必須、整数、0～2000の値
+       'img' => 'required',                          //必須
+       'user_id' => 'required|exists:users,id'       //必須、usersのidに存在
     );
 
 
     public function user()
     {
-       return $this->belongsTo('App\User');
+       return $this->belongsTo('App\User');          //belongsTo結合
     }
 }
